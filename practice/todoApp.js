8f44621423a371d77b2944d37413d350ca0f17b9
PovilasU <povilas.urbonas85@gmail.com>
2025-06-29 19:54:48 +0300

    chikd to parent example

--- practice/todoApp.js
+++ practice/todoApp.js
@@ -1,33 +1,56 @@
 import React, {useState} from 'react';
 
-functio TodoList(){
-	const [task,setTask]=useState('');
-	const [tasks,setTasks]=useState([]);
-	
-	const handleAddTask=()=>{
-		if(task.trim()==='')return;
-		setTasks([...tasks,task]);
-		setTask('');
-	};
-	return(
-		<div>
-			<input
-			type="text"
-			value={task}
-			onChange={(e)=>setTask(e.target.value)}
-			placeholder="Enter a task"
-			/>
-			<button onClick={handleAddTask}>Add </button>
-			
-			<ul>
-				{tasks.map((t,index)=>(
-					<li key={index}>{t}</li>
-				))}
-			</ul>
-		</div>
+function TodoList() {
+		const [task,setTask] = useState('');
+		const [tasks,setTasks] = useState([]);
 		
+		const handleAddTask=()=>{
+				if(task.trim()==='')return;
+				setTasks([...tasks,task]);
+				setTask('');
+		}
+		
+		const handleDeleteTask=(indexToDelete)=>{
+			
+			const newTasks=tasks.filter((_,index)=>indext!==indexToDelete);
+			setTask(newTasks);
+		}
+		
+		
+		const handleDeleteTask =(indextToDelete)=>{
+			const newTask=task.filter((_,index)=>index !==indexToDelete);
+			setTask(newTasks);
+		}
+		
+		return (
+			<div>
+				<h2> Todo List</h2>
+				<input
+				type="text"
+				value={task}
+				onChange={(e)=>setTask(e.target.value)}
+				placeholder="Enter a task"
+				/>
+				
+				<button onClick={handleAddTask> Add task </button>
+				
+				
+				<ul>
+					{tasks.map((t,index)=>(
+						<li key={index}>
+						{t}
+						<button onClick={()=>handleDeleteTask(index)}>Delete </button>
+						<button onClick={()=>handleDeleteTask(indext)}>delete </button>
+						</li>
+					))}
+				</ul>
+			
+			
+			</div>
+		
+		
+		);
 	
-	);
 }
 
 export default TodoList;
