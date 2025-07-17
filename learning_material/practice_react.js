6641f3992484ed8de8a4df6fc89bf7671e1ff1de
PovilasU <povilas.urbonas85@gmail.com>
2025-07-17 09:55:40 +0300

    add arrow function question

--- learning_material/practice_react.js
+++ learning_material/practice_react.js
@@ -1,75 +1,76 @@
-//1,if(2,3,4,5,6,
+//whoke App: 1,
+// interfaces:1,2,3,4,5,6,7,8,9,10
+// consts:1,2,3
+//interface and const1
 
-import React {useState,useEffect} from "react";
+//useEffect: 1,2,3,
+//prevpage nextPage: 1
+//return div:1
+
+
+
+import React, {useEffect,useState} from 'react';
+
 interface Joke{
 	id:string;
-	joke: string;
+	joke:string;
 }
 
-interface JokeResponse {
-	results: Joke[];
-	total_pages: number;
+interface JokesResponse {
+	jokes:Joke[];
+	total_pages:number;
 }
 
-const App: React.FC=()=>{
+const App:React.FC =()=>{
+	const [page,setPage] =useState<number>(1);
 	const [jokes,setJokes] = useState<Joke[]>([]);
-	const [page,setPage] = useState<number>(1);
 	const [totalPages,setTotalPages] = useState<number>(1);
 	const [loading,setLoading] = useState<boolean>(true);
 	
 	useEffect(()=>{
-	setLoading(true);
-	fetch(`http://icanhazdadjoke.com/search?page=${page}`,{
-		headers: {Accept: "application/json"},
-	}).
-	then((res)=>res.json()).
-	then((data:JokeResponse)=>{
-		setJokes(data.results);
-		setTotalPages(data.total_pages);
-		setLoading(false);
-	}).
-	catch((error)=>{
-		console.log("error message: ", error);
+		setLoading(true);
+		fetch(`https://icanhazdadjoke.com/search?page=${page}`,{
+			headers: {Accept: "application/json"},
+		}).
+		then((res)=>res.json()).
+		then((data:JokesResponse)=>{
+			setJokes(data.results);
+			setTotalPages(data.total_pages);
+			setLoading(false);
+		}).
+		catch((error)=>{
+			console.log("error while loading", error);
+			setLoading(false);
+		});
 		
-	});
 	},[page]);
 	
-	const nextPage = () =>{
-		if(page <totalPages) setPage((prev)=>prev+1);
+	const prevPage =() =>{
+		if(page>1) setPage((prev)=>prev-1);
 	};
-	const prePage=() => {
-	if(page>1) setPage((prev)=>prev-1);
+	const nextPage = ()=>{
+		if(page<totalPages) setPage((pre)=>prev+1);
 	};
+	
 
-	return (
+return(
 	<div>
-	<h1>Dad jokes (page {page}) </h1>
-	{loading ?(
-		<p>Loading... </p>
-	
-	):(
-	<ul>
-	{jokes.map((joke)=>(
-		<li key={joke.id}> {joke.joke} </li>
-	
-	))}
-	</ul>
-	)
-	
-	
-	}
-	
-	<button onClick={prevPage} disabled={page ===1}>
-	previous
-	
-	</button>
-	
-	<button onClick={nextPage} disabled={page ===totalPages}>
-	Next
-	
-	</button>
-	
+		<h1> Dad Jokes (page: ) </h1>
+		
+		{loading? (
+			<p>Loding...</p>
+		):(
+			<ul>
+				{jokes.map((joke)=>(
+					<li key={joke.id}>{joke}</li>
+				))}
+			</ul>
+		)}
+		
+		<button onClick={prevPage} disabled={page===1}> PreviousPage </button>
+		<button onClick={nextPage} disabled={page===total_pages}> NextPage </button>
 	</div>
-	)
-	
-}
+)
+};
+
+eport default App;
